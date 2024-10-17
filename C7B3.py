b9be3f452de7a657f5fc51b68e52c1319bcb5ce9
Nguyenhai64k2 <ngh79705@gmail.com>
2024-10-17 14:13:53 +0700

    Add files via upload

---
+++ C7B3.py
@@ -0,0 +1,9 @@
+print ("Sinh Vien : Nguyen Dinh Hai ")
+print ("MSSV: 235752021610028")
+print ("########################")
+def checkValue(n):
+    if n%2 == 0:
+            print ("Đây là một số chẵn")
+    else:
+            print("Đây là một số lẻ")
+checkValue(7)
