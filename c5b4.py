dc7f1f53c6bc22654af94f42ad8db1c6af214225
Nguyenhai64k2 <ngh79705@gmail.com>
2024-10-17 15:18:47 +0700

    Add files via upload

---
+++ c5b4.py
@@ -0,0 +1,10 @@
+print ("Sinh vien : Nguyen Dinh Hai ")
+print ("MSSV 23575202161028")
+print ("####################")
+ds=input("Danh sach:").split()
+# in cả dãy vừa nhập
+print(ds)
+# in dãy vừa nhập, mỗi phần tử trên một dòng
+for so in ds:
+    so=ds[::-1]
+print(so)
